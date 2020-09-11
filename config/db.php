b42696513fe638de54cba5180b15b38c48d670b9
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-09-10 20:45:56 -0500

    commit
    facturacion

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', 'root');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_invoice');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'DISTRIBUIDORA DE GAS "JESUS REY DE GLORIA" ');
+define('DIRECCION_EMPRESA', 'Isla Trinitaria Coop Independencia 1 mz239 sl.25');
+define('TELEFONO_EMPRESA', '+(593) 99 127 9013');
+define('EMAIL_EMPRESA', 'Alberto_romero1956@hotmail.com');
+define('TAX', '13');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
